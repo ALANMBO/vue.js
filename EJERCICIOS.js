bc6c4b44fe8c97417e9de8fb88458fa3f6119f9b
ALANMBO <bejaranomauricio999@gmail.com>
2024-07-06 17:13:42 -0400

    SE IMPLEMENTO NUEVOS CAMBIOS A LOS EJERCICIOS

--- EJERCICIOS.js
+++ EJERCICIOS.js
@@ -1,3 +1,45 @@
+// VARIABLES
+/* Ejercicio 1: El Cofre de las Gemas
+Imagina que tienes un cofre con tres tipos de gemas: rubíes, zafiros y 
+esmeraldas. Cada gema tiene un valor diferente en monedas de oro: los rubíes
+valen 10 monedas, los zafiros valen 5 monedas y las esmeraldas valen 2 
+monedas. Si tienes 10 gemas en total y el valor total en el cofre es 
+de 75 monedas de oro, ¿cuántas gemas de cada tipo tienes? */
+
+var rubies = 10 , zafiros = 5 ,esmeraldas = 2
+
+var cantidadRubies = 7, cantidadZafiros = 1 ,  cantidadEsmeraldas = 2
+
+var sumaGemas = rubies * cantidadRubies + zafiros * cantidadZafiros + esmeraldas * cantidadEsmeraldas;
+
+document.write("EL VALOR TOTAL ES: " + sumaGemas + "<br>")
+
+
+/* Ejercicio 2: La Tienda de Postres
+Imagina que estás administrando una tienda 
+de postres que vende tres tipos de dulces: 
+pasteles, galletas y helados. Cada tipo de 
+dulce tiene un precio diferente:
+
+Los pasteles cuestan 20 monedas cada uno.
+Las galletas cuestan 5 monedas cada una.
+Los helados cuestan 10 monedas cada uno.
+En un día, vendiste un total de 50 dulces y 
+el ingreso total fue de 500 monedas. 
+¿Cuántos pasteles, galletas y helados vendiste? */
+
+var pasteles =  20 ,  galletas = 5, helados = 10 , sumaDePostres = 0 
+//ingresoTotal = 500
+// dulce de pastel = 20 monedas * 10  = 200
+// dulces de galletas = 5 monedas * 20 = 100
+//dulces de helados  = 10 monedas * 20 = 200
+
+sumaDePostres = pasteles * 10 + galletas * 20 + helados * 20
+document.write("LA SUMA DE LOS POSTRES ES DE :" + sumaDePostres) 
+
+
+
+
 //ASIGNACIÓN, CONSOLE LOG y TYPEOF en JAVASCRIPT 
 
 /*Asignación de Variables Básica:
@@ -7,7 +49,7 @@ Asigna el valor 20 a una variable llamada b.
 Asigna el valor a + b a una variable llamada c. */
 var a = 10 , b = 20 , c 
 c = a + b 
-console.log("LA SUMA ES : "+ typeof c)
+console.log("LA sumatotaldecofre ES : "+ typeof c)
 
 /* Asignación de Cadenas de Texto:
 
@@ -29,13 +71,13 @@ var esFalso = false
 
 var comida = 2800  
 var comida1 = 1300
-let sumadecomida = comida + comida1
+let sumatotaldecofredecomida = comida + comida1
 var bebida = 900
 var bebida1 = 650
-let sumadeBebida = bebida + bebida1
+let sumatotaldecofredeBebida = bebida + bebida1
 
-console.log("LA COMIDA ES $:" + sumadecomida)
-console.log("LA BEBIDA ES $:" + sumadeBebida)
+console.log("LA COMIDA ES $:" + sumatotaldecofredecomida)
+console.log("LA BEBIDA ES $:" + sumatotaldecofredeBebida)
 
 /* Ejercicio 1: Comparación de Números:
 Imagina que tienes dos números enteros, A y B. Escribe un ejercicio que muestre por pantalla
@@ -93,14 +135,14 @@ switch(dia){
 
 /* Ejercicio con bucle for:
 Escribe un bucle for que recorra los números del 1 al 10 y 
-sume todos los números pares. Al final, debe imprimir la suma de estos números pares. */
-let suma = 0
+sume todos los números pares. Al final, debe imprimir la sumatotaldecofre de estos números pares. */
+let sumatotaldecofre = 0
 for(let i = 0 ; i <= 10 ; i++){
     if(i % 2 == 0 ){
-        suma += i
+        sumatotaldecofre += i
     }
 }
-console.log(suma)
+console.log(sumatotaldecofre)
 
 /* Factorial de un número: Calcula el factorial de un número dado utilizando un bucle while. 
 El factorial de un número n se define como el producto de todos los enteros positivos menores o iguales a n. */
